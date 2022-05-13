7646a8565b1a4f130a8cbcb527f3196323583433
andyrod <andyrod209@gmail.com>
2022-05-13 10:10:10 -0700

    Chore: Created a Modal for adding a new song.

--- src/Components/SongForm/SongForm.jsx
+++ src/Components/SongForm/SongForm.jsx
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { useState } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import './SongForm.css'
 
 const SongForm = (props) => {
 
@@ -8,9 +11,12 @@ const SongForm = (props) => {
     const [album, setAlbum] = useState('')
     const [genre, setGenre] = useState('')
     const [releaseDate, setRelease_Date] = useState('')
-    
-  
 
+    // modal variables
+    const [show, setShow] = useState(false);
+    
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     // add new song to list of songs
     async function createSong(){
         let song = {
@@ -24,28 +30,43 @@ const SongForm = (props) => {
         let response = await axios.post('http://127.0.0.1:8000/music/', song);
             await props.getAllMusic();
             console.log(response);
-            console.log(response.data);
-        
+            console.log(response.data);   
     }
 
     // prevents from refreshing page
     function handleSubmit(formEvent){
         formEvent.preventDefault();
         createSong();
+        handleClose();
         }
         
-   
-    
     return ( 
-        <form onSubmit={handleSubmit}>
-            <h3>Add New Song</h3>
-            <input className="newSong" type='text' placeholder="Songname" onChange={(event) => setTitle(event.target.value)}/>
-            <input className="newSong" type='text' placeholder="Album" onChange={(event) => setArtist(event.target.value)}/>
-            <input className="newSong" type='text' placeholder="Artist" onChange={(event) => setAlbum(event.target.value)}/>
-            <input className="newSong" type='text' placeholder="Genre" onChange={(event) => setGenre(event.target.value)}/>
-            <input className="newSong" type='date' placeholder="Date" onChange={(event) => setRelease_Date(event.target.value)}/>
-            <button type ="submit">Add</button>
-        </form>
+        <>
+        <Button variant="primary" onClick={handleShow}>Add New Song</Button>
+        <Modal show={show} onHide={handleClose} animation={false}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add New Song</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <form className="song-form" onSubmit={handleSubmit}>
+            <label>Title: </label>
+                <input className="newSong" type='text' placeholder="Songname" onChange={(event) => setTitle(event.target.value)} />
+            <label>Album: </label>
+                <input className="newSong" type='text' placeholder="Album" onChange={(event) => setArtist(event.target.value)} />
+            <label>Artist: </label>
+                <input className="newSong" type='text' placeholder="Artist" onChange={(event) => setAlbum(event.target.value)} />
+            <label>Genre: </label>    
+                <input className="newSong" type='text' placeholder="Genre" onChange={(event) => setGenre(event.target.value)} />
+            <label>Release Date: </label>
+                <input className="newSong" type='date' placeholder="Date" onChange={(event) => setRelease_Date(event.target.value)} />
+            </form>
+        </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>Close</Button>
+                <Button variant="primary" onClick={handleSubmit} value="Submit" type="submit">Save Changes</Button>
+            </Modal.Footer>
+        </Modal>
+        </>
      );
 }
  
